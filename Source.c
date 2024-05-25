1b317d8556805ac2ab2a374f0b5fb9cfa7074b61
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2024-05-25 23:26:16 +0300

    .5

--- Source.c
+++ Source.c
@@ -65,11 +65,119 @@ int dequeue(Queue* q)
     return value;
 }
 
+//
+void merge(Queue* q, int left, int mid, int right)
+{
+    int size1 = mid - left + 1;
+    int size2 = right - mid;
+
+    //int L[size1], R[size2];
+    int* L = (int*)malloc(size1 * sizeof(int));
+    int* R = (int*)malloc(size2 * sizeof(int));
+
+    for (int i = 0; i < size1; i++)
+    {
+        L[i] = q->data[(left + i) % MAX_SIZE];
+    }
+    for (int j = 0; j < size2; j++)
+    {
+        R[j] = q->data[(mid + 1 + j) % MAX_SIZE];
+    }
+
+    int i = 0, j = 0, k = left;
+    while (i < size1 && j < size2)
+    {
+        if (L[i] <= R[j])
+        {
+            q->data[k % MAX_SIZE] = L[i];
+            i++;
+        }
+        else
+        {
+            q->data[k % MAX_SIZE] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < size1)
+    {
+        q->data[k % MAX_SIZE] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < size2)
+    {
+        q->data[k % MAX_SIZE] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSortUtil(Queue* q, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        mergeSortUtil(q, left, mid);
+        mergeSortUtil(q, mid + 1, right);
+        merge(q, left, mid, right);
+    }
+}
+
+void mergeSort(Queue* q)
+{
+    mergeSortUtil(q, q->front, q->rear);
+}
+//
+
+void fillQueue(Queue* q)
+{
+    //открытие файла
+    FILE* file = fopen("input.txt", "r");
+    if (!file)
+    {
+        printf("Unable to open file\n");
+        return 1;
+    }
+
+    //заполнение очереди
+    int num;
+    while (fscanf(file, "%d", &num) == 1)
+    {
+        enqueue(&q, num);
+    }
+
+    fclose(file);
+}
+
+/*void printQueue(Queue* q, char message[64])
+{
+    printf("%s:\n\n", message);
+    while (!isEmpty(&q))
+    {
+        printf("%d ", dequeue(&q));
+    }
+}*/
+
+void printQueue(Queue q, char message[64])
+{
+    printf("%s:\n\n", message);
+    int i = q.front;
+    while (i % MAX_SIZE != q.rear) {
+        printf("%d ", q.data[i]);
+        i = (i + 1) % MAX_SIZE;
+    }
+    printf("%d\n\n", q.data[q.rear]);
+}
+
 int main()
 {
     //открытие файла
     FILE* file = fopen("input.txt", "r");
-    if (!file) {
+    if (!file)
+    {
         printf("Unable to open file\n");
         return 1;
     }
@@ -80,17 +188,16 @@ int main()
 
     //заполнение очереди
     int num;
-    while (fscanf(file, "%d", &num) == 1) {
+    while (fscanf(file, "%d", &num) == 1)
+    {
         enqueue(&q, num);
     }
 
     fclose(file);
-
-    // Пример вывода содержимого очереди
-    printf("Raw Queue:\n\n");
-    while (!isEmpty(&q)) {
-        printf("%d ", dequeue(&q));
-    }
+    printQueue(q, "Raw queue");
+    
+    mergeSort(&q);
+    printQueue(q, "Sorted queue");
 
     printf("\n\n");
     return 0;
